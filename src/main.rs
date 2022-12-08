50d0b4373b906eddf86c4c0117a72626d4e430ed
NeoTech <andreas@roughedge.se>
2022-12-08 10:47:23 +0100

    More test code to the mill

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 mod mods;
-use mods::math::{add, sub, mandelbrot};
-use mods::login::{login};
-use mods::highlighter::{_highlight_countries};
+use mods::math::*;
+use mods::login::*;
+use mods::highlighter::*;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: client <command> [<args>]");
-        eprint!("Commands: greet, add, subtract, login, highlight_countries, ncurses(linux), mandelbrot");
+        eprint!("Commands: greet, add, subtract, login, highlight_countries, ncurses(linux), mandelbrot, julia");
         return;
     }
 
@@ -22,6 +22,7 @@ fn main() {
         "sub" => sub(command_args),
         "login" => login(command_args),
         "mandelbrot" => mandelbrot(command_args),
+        "julia" => julia(command_args),
         #[cfg(target_os = "linux")]
         "ncurses" => ncurses(command_args),
         "highlight_countries" => _highlight_countries(command_args),
--- src/mods/math.rs
+++ src/mods/math.rs
@@ -25,6 +25,62 @@ use std::io::Write;
 use std::fs::File;
 use std::path::Path;
 
+pub fn julia(args: &[String]) {
+    if args.len() < 2 {
+        eprintln!("Usage: julia <width> <height>");
+        return;
+    }
+    let width = args[0].parse::<usize>().unwrap();
+    let height = args[1].parse::<usize>().unwrap();
+    let mut buffer = vec![0u8; width * height];
+    let mut file = File::create(Path::new("julia.txt")).unwrap();
+    let mut x = 0;
+    let mut y = 0;
+    while y < height {
+        while x < width {
+            let cx = x as f64 * 3.5 / width as f64 - 2.5;
+            let cy = y as f64 * 2.0 / height as f64 - 1.0;
+            let mut zx = 0.0;
+            let mut zy = 0.0;
+            let mut i = 0;
+            while zx * zx + zy * zy < 4.0 && i < 50 {
+                let xtemp = zx * zx - zy * zy + cx;
+                zy = 2.0 * zx * zy + cy;
+                zx = xtemp;
+                i += 1;
+            }
+            buffer[y * width + x] = i as u8;
+            x += 1;
+        }
+        y += 1;
+        x = 0;
+    }
+    let mut y = 0;
+    while y < height {
+        let mut x = 0;
+        while x < width {
+            let i = buffer[y * width + x];
+            if i == 50 {
+                file.write(b"*").unwrap();
+            } else {
+                file.write(b" ").unwrap();
+            }
+            x += 1;
+        }
+        file.write(b"
+
+").unwrap();
+        y += 1;
+    }
+}
+
+#[test]
+fn julia_test() {
+    assert_eq!(julia(&vec!["40".to_string(), "40".to_string()]), ());
+}
+
+/* A function that generates a mandelbrot fractal in ascii output */
+
 pub fn mandelbrot(args: &[String]) {
     if args.len() < 2 {
         eprintln!("Usage: mandelbrot <width> <height>");
