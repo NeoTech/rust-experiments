84f04a77d32ac3f39cce3ecfae5e42c5ed599262
NeoTech <andreas@roughedge.se>
2022-12-12 21:10:35 +0100

    Changed commenting.

--- src/mods/binarymask.rs
+++ src/mods/binarymask.rs
@@ -6,7 +6,10 @@ pub fn frommask(args: &[String]) {
     /* Function that convers a number to a array of 1 and 0 */
     println!("Converting {} to binary", args[0]);
     let mask = args[0].parse::<i32>().unwrap();
-    let a = format!("{:b}", mask).chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<u32>>();
+    let a = format!("{:b}", mask)
+        .chars()
+        .map(|c| c.to_digit(10).unwrap()
+        ).collect::<Vec<u32>>();
     /* Adds padding to Vector until it represents a 32 bit number */
     let mut b = a.clone();
     while b.len() < 32 {
@@ -23,7 +26,14 @@ pub fn tomask(args: &[String]) {
     }
     /* Function that convers a array of 1 and 0 to a number */
     /* ie 0,1,0 */
-    let a = args[0].split(",").map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-    let mask = u32::from_str_radix(&a.iter().map(|&i| i.to_string()).collect::<String>(), 2).unwrap();
+    let a = args[0].split(",")
+        .map(|s| s.parse::<u32>()
+        .unwrap())
+        .collect::<Vec<u32>>();
+    let mask = u32::from_str_radix(&a.iter()
+            .map(|&i| i
+            .to_string())
+        .collect::<String>(), 2)
+        .unwrap();
     println!("{:?}", mask);
 }
